866d6e9e8244a3990e153ce6dcda5bda409fa560
shanky <shankybnl@gmail.com>
2015-12-06 22:06:52 +0530

    added libs and tests folder

---
+++ src/Outfittery/web/tests/HomePageTest.java
@@ -0,0 +1,33 @@
+package Outfittery.web.tests;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import Outfittery.web.helpers.HomeHelper;
+import UITestFramework.CreateSession;
+
+public class HomePageTest extends CreateSession{
+
+	HomeHelper homeHelper;
+	String email;
+	String password;
+	String baseUrl;
+	
+	@BeforeMethod(alwaysRun = true)
+	public void instantiateHelpers(){
+		email = mwNewFlowProperties.getString("email");
+		password = mwNewFlowProperties.getString("password");
+		baseUrl = mwNewFlowProperties.getString("baseUrl");
+		homeHelper = new HomeHelper(driver,baseUrl);
+	
+	}
+	
+	
+	
+	
+	@Test(priority=0)
+	public void logintest() throws InterruptedException{
+		homeHelper.login(email, password);
+		homeHelper.profile();
+	}
+}
